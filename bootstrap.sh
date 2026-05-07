a5d2561f581e6f00d587606f3d9d1b75c2395c2d
Vincent Tee <vincentminghan26@gmail.com>
2026-05-07 10:07:36 +0800

    Add oh-my-zsh config and install plugins in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,24 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
+function installOhMyZsh() {
+	if [ ! -d "$HOME/.oh-my-zsh" ]; then
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
+	fi;
+
+	ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}";
+
+	if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+		git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions";
+	fi;
+
+	if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting";
+	fi;
+}
+
 function doIt() {
+	installOhMyZsh;
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
